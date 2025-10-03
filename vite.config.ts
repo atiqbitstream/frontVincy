815a49f6154e88b65f7decec976bf8a3f5a47e35
atiqbitstream <atiq.bitstream@gmail.com>
2025-10-03 17:41:50 +0500

    build error fix 3

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
@@ -24,23 +23,12 @@ export default defineConfig(({ mode }) => ({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) {
-              return 'react';
-            }
-            if (id.includes('@radix-ui')) {
-              return 'radix';
-            }
-            if (id.includes('lucide-react')) {
-              return 'icons';
-            }
-            return 'vendor';
-          }
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'ui-vendor': ['@radix-ui/react-slot', '@radix-ui/react-dialog'],
         },
       },
     },
-    chunkSizeWarningLimit: 2000,
-    target: 'esnext',
+    chunkSizeWarningLimit: 1000,
   },
 }));
